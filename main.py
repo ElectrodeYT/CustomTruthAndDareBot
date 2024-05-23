c2f79c6770489c9eadf7fe95f531c9ee7623865c
Alexander <electrodeyt@gmail.com>
2024-05-23 02:19:12 +0200

    allow use of env variables for prefix and token

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import os
+
 import discord, json
 from discord.ext import commands
 from truthdare import TruthDareCog
 
-with open("config.json", "r") as f:
-    config = json.loads(f.read())
+if os.path.exists('config.json'):
+    with open('config.json', 'r') as f:
+        config = json.loads(f.read())
+else:
+    config = {}
+
+if os.environ.get('BOT_TOKEN') is not None:
+    config['botToken'] = os.environ['BOT_TOKEN']
+
+if os.environ.get("BOT_PREFIX") is not None:
+    config['prefix'] = os.environ['BOT_PREFIX']
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix=config["prefix"], intents=intents)
+bot = commands.Bot(command_prefix=config['prefix'], intents=intents)
 
 
 @bot.event
@@ -18,7 +29,7 @@ async def on_ready():
 
 
 def main():
-    bot.run(config["botToken"])
+    bot.run(config['botToken'])
 
 
 if __name__ == '__main__':
